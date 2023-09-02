057134a6237e7993475fed32a331fa437199e7c0
tot.chow <totchow@gmail.com>
2023-09-02 11:30:16 +0800

    test update

---
+++ Demo-helloworld/src/Helloworld.java
@@ -0,0 +1,10 @@
+// head of the file
+public class Helloworld {
+  // This is comment
+  public static void main (String[] args) {
+    // Program starts
+    // Tool: System.out.println
+    System.out.println("Hello world!!!你好!!");
+    System.out.println("123ABC");
+  }
+}
---
+++ demo-datatype/src/DataType.java
@@ -0,0 +1,13 @@
+public class DataType {
+  public static void main(String[] args) {
+
+    //Assign value "Hello World!!!" to a variable str
+    String box = "Hello World!!!"; 
+    System.out.println(box); //Print value of variable
+    System.out.println("box"); //Print exact String
+
+    String str; //Declare String
+    str = "abc"; //Assign value to variable
+    System.out.println(str); //abc
+  }
+}
